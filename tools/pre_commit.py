5ee8b4d6882c2c63b42ec96c98b8057efbfe1c6e
Pedro Algarvio <palgarvio@vmware.com>
2023-07-02 11:39:31 +0100

    Start running actionlint if available
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

---
+++ tools/pre_commit.py
@@ -0,0 +1,50 @@
+# Copyright 2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+"""
+These commands are used by pre-commit.
+"""
+# pylint: disable=resource-leakage,broad-except,3rd-party-module-not-gated
+from __future__ import annotations
+
+import shutil
+from typing import NoReturn
+
+from ptscripts import command_group
+from ptscripts import Context
+
+
+# Define the command group
+cgroup = command_group(name="pre-commit", help="Pre-Commit Related Commands", description=__doc__)
+
+
+@cgroup.command(  # type: ignore[misc]
+    name="actionlint",
+    arguments={
+        "files": {
+            "help": "Files to run actionlint against",
+            "nargs": "*",
+        },
+        "no_color": {
+            "help": "Disable colors in output",
+        },
+    },
+)
+def actionlint(ctx: Context, files: list[str], no_color: bool = False) -> NoReturn:
+    """
+    Run `actionlint`.
+    """
+    actionlint = shutil.which("actionlint")
+    if not actionlint:
+        ctx.warn("Could not find the 'actionlint' binary")
+        ctx.exit(0)
+    cmdline = [actionlint]
+    if no_color is False:
+        cmdline.append("-color")
+    shellcheck = shutil.which("shellcheck")
+    if shellcheck:
+        cmdline.append(f"-shellcheck={shellcheck}")
+    pyflakes = shutil.which("pyflakes")
+    if pyflakes:
+        cmdline.append(f"-pyflakes={pyflakes}")
+    ret = ctx.run(*cmdline, *files, check=False)
+    ctx.exit(ret.returncode)
